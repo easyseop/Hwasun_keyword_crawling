93477fece019e25886e0bc60dd9d3d9516195e76
JISEOP LEE <71008721+easyseop@users.noreply.github.com>
2022-04-23 23:14:18 +0900

    0423

--- cv.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0,'/chromedriver')
-import pandas as pd
-import requests
-import urllib.request as ur
-import time
-from bs4 import BeautifulSoup as bs
-from selenium import webdriver
-from tqdm import tqdm
--- daum_crawling.py
+++ daum_crawling.py
@@ -1,3 +1,4 @@
+from text_crawling import soup_find_all
 def scrape_daum(soup):
     res = soup_find_all(soup, 'div', {'class':'tt_article_useless_p_margin contents_style'})
     if res == True:
